ce7e2973d5cf7efcbc26de4e127e4ab6f7ea8668
ArturRL <artur.rod.lopes@gmail.com>
2022-08-11 20:17:52 -0300

    Desafio Entregue

---
+++ script.js
@@ -0,0 +1,27 @@
+let dados=["musica","jogos","comida"]
+console.log(dados)
+
+////
+
+dados.unshift("monitoria")
+console.log(dados)
+
+////
+
+dados.pop()
+console.log(dados)
+
+////
+
+dados.push("pao","mortandela")
+console.log(dados)
+
+////
+
+dados.shift()
+console.log(dados)
+
+////
+
+let number=[5,9,7,6,3,4,1,2,8]
+console.log(number.sort())
